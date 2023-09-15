03e78cac808852d4e8ca1d514066fcf611b43170
kelton zhang <kelton@xetdata.com>
2023-09-15 11:56:11 -0700

    limit entry path to only artifact store and call updated xet_artifact module

--- setup.py
+++ setup.py
@@ -7,13 +7,6 @@ setup(
     # the plugin and then immediately use it with MLflow
     install_requires=["mlflow"],
     entry_points={
-        "mlflow.tracking_store": "file-plugin=mlflow_xet_plugin.file_store:PluginFileStore",
-        "mlflow.artifact_repository": "file-plugin=mlflow_xet_plugin.local_artifact:PluginLocalArtifactRepository",
-        "mlflow.run_context_provider": "unused=mlflow_xet_plugin.run_context_provider:PluginRunContextProvider",
-        "mlflow.request_header_provider": "unused=mlflow_xet_plugin.request_header_provider:PluginRequestHeaderProvider",
-        "mlflow.model_registry_store": "file-plugin=mlflow_xet_plugin.sqlalchemy_store:PluginRegistrySqlAlchemyStore",
-        "mlflow.project_backend": "unused=mlflow_xet_plugin.dummy_backend:PluginDummyProjectBackend",
-        "mlflow.deployments": "unused=mlflow_xet_plugin.fake_deployment_plugin",
-        "mlflow.model_evaluator": "unused=mlflow_xet_plugin.dummy_evaluator:DummyEvaluator",
+        "mlflow.artifact_repository": "xet=mlflow_xet_plugin.xet_artifact:XetHubArtifactRepository",
     },
 )
