2473ce42cf02b1a37d94374d3f9452ef93b8a6eb
kelton zhang <kelton@xetdata.com>
2023-09-22 17:32:35 -0700

    using active run as fixture

--- tests/test_mlflow_xetstore.py
+++ tests/test_mlflow_xetstore.py
@@ -16,39 +16,9 @@ from mlflow.entities import (Experiment, Run, RunInfo, RunData, RunTag, Metric,
 
 @pytest.fixture # run before each test function to which it is applied
 def run():
-    # experiment = Experiment(experiment_id="1",
-    #                         name="experiment_name",
-    #                         artifact_location="artifact_location",
-    #                         lifecycle_stage=LifecycleStage.ACTIVE,
-    #                         tags=[])
-    experiment_id = create_experiment(random_string(10))
-    
-    run_info = RunInfo(
-        run_uuid="1",
-        run_id="1",
-        experiment_id=experiment_id,
-        user_id="unknown",
-        status=RunStatus.to_string(RunStatus.RUNNING),
-        start_time=1,
-        end_time=None,
-        lifecycle_stage=LifecycleStage.ACTIVE,
-        artifact_uri=artifact_uri
-    )
-
-    run_data = RunData(metrics=[], params=[], tags=[])
-    run = Run(run_info=run_info, run_data=run_data)
-
-    metric = Metric(key="metric1", value=1, timestamp=1, step=1)
-
-    param = Param(key="param1", value="val1")
-
-    tag = RunTag(key="tag1", value="val1")
-
-    experiment_tag = ExperimentTag(key="tag1", value="val1")
-
     # start_mlflow_server_for_xethub()
-
-    return run
+    with mlflow.start_run() as run:
+        yield run
 
 def test_user_info():
     user = os.getenv('XET_TEST_USER')
@@ -60,7 +30,7 @@ def test_user_info():
 
     host = os.getenv('XET_ENDPOINT')
     if host is None:
-        host = 'https://xethub.com'
+        host = 'https://hub.xetsvc.com/'
 
     return {
         "user": user,
@@ -69,16 +39,17 @@ def test_user_info():
         "host": host,
     }
 
-def test_account_login():
-    user_info = test_user_info()
-    pyxet.login(user_info['user'], user_info['token'], user_info['email'], user_info['host'])
-    return user_info['user']
+# def test_account_login():
+#     user_info = test_user_info()
+    # pyxet.login(user_info['user'], user_info['token'], user_info['email'], user_info['host'])
+#     return user_info['user']
 
 # Expect a test repo whose main branch is empty (only .gitattributes)
 def test_repo():
     repo = os.getenv('XET_TEST_REPO')
     assert repo is not None
-    user = test_account_login()
+    user_info = test_user_info()
+    user = user_info['user']
     assert user is not None
     # set by XET_TEST_REPO
     repo_url = f"xet://{user}/{repo}"
@@ -132,8 +103,8 @@ def start_mlflow_server_for_xethub():
         print("Error:", e)
 
 def test_get_artifact_uri(run):
-    artifact_uri = run.info.artifact_uri 
-    assert(get_artifact_uri()==artifact_uri)
+    assert(mlflow.active_run())
+    assert(get_artifact_uri()==run.info.artifact_uri)
 
 def test_mlflowClient_list_artifacts(run):
     artifact_uri = run.info.artifact_uri 
